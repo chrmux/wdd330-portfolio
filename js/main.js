2f8e797347bf4eb35a4e3d45ae1dd33bd24a71a1
sol21002@byui.edu <85121697+Alainsol@users.noreply.github.com>
2022-04-20 10:50:19 +0700

    update week1

---
+++ js/main.js
@@ -0,0 +1,18 @@
+const links = [
+    {
+      label: "Week1 notes",
+      url: "week-1/index.html"
+    }
+  ];
+
+const worklist = document.getElementById('listwork');
+
+for(let i = 0; i < links.length; i++) {
+    const menu = document.createElement('li');
+    const link = document.createElement('a');
+    link.href = links[i].url;
+    link.innerHTML = links[i].label;
+    menu.appendChild(link);
+    worklist.appendChild(menu);
+}
+              
