0e962d00141a4126c128b656db5e44161e71a665
sol21002@byui.edu <85121697+Alainsol@users.noreply.github.com>
2022-05-21 14:52:28 +0700

    update toggle

--- week-5/js/theme.js
+++ week-5/js/theme.js
@@ -2,6 +2,7 @@ let theme = document.getElementById('theme');
 
 theme.onclick = () => {
   theme.classList.toggle('fa-sun');
+  theme.classList.toggle('fa-moon');
 
   if (theme.classList.contains('fa-sun')) {
     document.body.classList.add('active');
