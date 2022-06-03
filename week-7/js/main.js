c9d15523b09963be5e2e0eccda73f1d4daa26c4e
sol21002@byui.edu <85121697+Alainsol@users.noreply.github.com>
2022-06-03 11:33:25 +0700

    update

--- week-7/js/main.js
+++ week-7/js/main.js
@@ -1,7 +1,9 @@
-import Hikes from './hikes.js';
-import Comment from './comments.js';
-const myHikes = new Hikes('hikes');
-console.log(Comments);
-window.addEventListener('load', () => {
 
+import Hikes from './hikes.js';
+import Comment from './comments.js'
+const myHikes = new Hikes('hikes');
+const comments = new Comment('comments');
+console.log(comments);
+window.addEventListener('load', () => {
+  myHikes.showHikeList();
 });
