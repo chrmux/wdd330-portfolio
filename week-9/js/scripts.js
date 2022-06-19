d6e6a6faef7acc4f0dae17176ce984ea1c46323d
sol21002@byui.edu <85121697+Alainsol@users.noreply.github.com>
2022-06-19 12:51:54 +0700

    update

--- week-9/js/scripts.js
+++ week-9/js/scripts.js
@@ -1,15 +1,26 @@
-/*var square = document.getElementById('square');
-var angle = 0;
-setInterval(function() {
-angle = (angle + 5)%360
-square.style.transform = 'rotate(' + angle + 'deg)'
-}, 1000/6)*/
+/*source https://anyflip.com/omgk/ajlm/basic/351-400*/
 
-var square = document.getElementById('square');
-var angle = 0;
+
+
+const squareElement = document.getElementById('square');
+let angle = 0;
 function rotate() {
-angle = (angle + 5)%360
-square.style.transform = 'rotate(' + angle + 'deg)'
-window.requestAnimationFrame(rotate);
-}
-id = window.requestAnimationFrame(rotate)
+    angle = (angle + 2)%360;
+    squareElement.style.transform = `rotate(${angle}deg)`
+    window.requestAnimationFrame(rotate);
+    }
+console.log('square');
+
+
+const squareElement2 = document.getElementById('square2');
+let angle2 = 0;
+setInterval( () => {
+    angle2 = (angle2 + 2) % 360;
+    squareElement.style.transform = `rotate(${angle2}deg)`
+}, 1000/60);
+console.log('square2');
+
+const id = requestAnimationFrame(rotate);
+cancelAnimationFrame(id); // to return the unique IT to cancel the animation
+
+rotate();
